6a3c5b2e0f2bab0d598dc17a5418045e457d64ef
bconsole <beatconsole@googlemail.com>
2012-02-01 19:12:13 +0000

    jsdom testing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http 			= require('http')
-		contextify = require('contextify');
+		contextify = require('contextify'),
+		jsdom = require('jsdom');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
