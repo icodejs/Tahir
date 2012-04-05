c54bd22a9cd92d43584d296f173d5e3d02f73ca7
icodejs <icodejs@gmail.com>
2012-04-05 08:43:25 +0100

    Added useful info to README and updated tahir.no.de

---
+++ public/cv/cv_template.js
@@ -0,0 +1,76 @@
+exports.cv = {
+		fullname: 	'Tahir Joseph',
+		position: 	'Javascript web application developer',
+		telephone: 	'+44 7527 029 380',
+		email: 			'info@icodejs.com',
+		profilePic:	'/img/cv/profile.jpg',
+		websites: [
+			'icodejs.no.de',
+			'icodejs.com',
+			'blog.icodejs.com',
+			'datadial.net/the-team/tyrone/'
+		],
+		skills: [
+			{ name: 'HTML(5)', level: 0 },
+			{ name: 'CSS(3)', level: 0 },
+			{ name: 'Javascript', level: 0 },
+			{ name: 'JQuery', level: 0 },
+			{ name: 'Node.JS', level: 0 },
+			{ name: 'VB', level: 0 },
+			{ name: 'Asp.Net', level: 0 },
+			{ name: 'SQL(Server)', level: 0 }
+		],
+		socialLinks: [
+			{ name: 'Delicious', 	url: 'http://www.delicious.com/icodejs' },
+			{ name: 'GitHub', 		url: 'https://github.com/bconsole' },
+			{ name: 'Twitter', 		url: '@1codejs'}
+		],
+		experience: [
+			{
+				name: 				'Datadial SEO App',
+				url:					'/',
+				description: 	'Provide images and a explanation'
+			},
+			{
+				name: 				'The Running school',
+				url:					'datadial.net/case-studies/running-school/',
+				description: 	''
+			},
+			{
+				name: 				'The Running school',
+				url:					'datadial.net/case-studies/splendiferous/',
+				description: 	''
+			},
+									{
+				name: 				'Popkorn Communications',
+				url:					'datadial.net/case-studies/popkorn-communications/',
+				description: 	''
+			},
+			{
+				name: 				'Marvic Textiles',
+				url:					'datadial.net/case-studies/marvic-textiles/',
+				description: 	''
+			}
+		],
+		profile: '<p>I originally came from a Windows desktop application background' +
+							'where he worked on numerous projects building and maintaining' +
+							'applications used by Channel 4, Sky and the BBC. I move to' +
+							'datadial in 2007 where I was able to take my current .Net skills' +
+							'to web development.</p>' +
+							'<p>At Datadial I learnt how to build and maintain web applications' +
+							'Javascript, JQuery, Ajax, VB, Asp.net, SQL (server), C# (basic)' +
+							'and LINQ. <a href="http://www.datadial.net/case-studies/?teamMemberID=9" target="_blank">View' +
+							'past work</a></p>' +
+							'<p>My current focus is to solely on HTML5, CSS3, Javascript,' +
+							'JQuery and Node.JS. The reason for this switch is due to the' +
+							'overwelming shift in the need for platform independant web' +
+							'applictions and the ability to use one language on the client and' +
+							'the server.</p>',
+		education: [
+			'BTEC HND (London Metropolitan University)',
+			'BSC Computing : Grade 2.1 (London Metropolitan University)'
+		],
+		interests: [
+			'Weight training', 'Wing chun martial arts', 'Music production'
+		]
+	};
